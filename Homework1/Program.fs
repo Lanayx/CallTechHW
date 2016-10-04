db84e5ecc60c8a2d2ee0cee1ccd78083bb7044c1
Vladimir Shchur <vladimir.shchur@idt.net>
2016-10-04 12:12:34 +0300

    Temp changes

--- Homework1/Program.fs
+++ Homework1/Program.fs
@@ -1,46 +1,75 @@
 ﻿open System
 open FSharp.Charting
+open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.LinearAlgebra.Double
 open MathNet.Numerics.Distributions
 
+let N = 10
+let mutable counter = 0
+let linePoints = [|for i in 1..2 -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0) )|]
+let dataPoints = [for i in 1..N -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0) )]
+
+
+let getrandomitem() =
+  let rnd = System.Random()
+  fun (combos : string list) -> List.nth combos (rnd.Next(combos.Length))
+
+let label_point(line: Vector<float>, point: Vector<float>) =
+    let point3 = Vector.Build.Dense([|1.0;point.[0];point.[1]|])
+    if line*point3 > 0.0 then (point,1.0) else (point,-1.0)
+
+let find_line(point1: Vector<float>, point2: Vector<float>) =
+
+//    """
+//    Finds the line crossing `point_1` and `point_2`. Returns an array
+//    [w0, w1, w2] representing a line: w0 + w1 * x + w2 * y = 0.
+//    `point_1` and `point_2` are 2-element arrays.
+//    """
+//    # [x1 y1] . [w1] = [-w0] ==> a . x = b
+//    # [x2 y2]   [w2]   [-w0]
+//look at http://numerics.mathdotnet.com/LinearEquations.html
+
+    a = [point_1, point_2]
+    w0 = 1.
+    b = [-w0, -w0]
+    w1, w2 = np.linalg.solve(a, b)
+    return np.array([w0, w1, w2])
+
+
+let rec learn (misclasified: (Vector<float>*float) list) (w: Vector<float>) correctLabels =
+        match misclasified with
+            | [] -> None
+            | (x,s)::t ->
+                counter <- counter + 1
+                let newW = w + Vector.Build.Dense([|1.0;x.[0];x.[1]|])*s
+                let classifiedNew =  dataPoints |> List.map (fun v -> label_point(newW, v))
+                let msklNew = List.zip correctLabels classifiedNew
+                               |> List.filter (fun(y,(x,s)) -> y <> s)
+                               |> List.map (fun (y, x) -> x)
+                learn msklNew newW correctLabels
+
+let perceptron_learning correctLabels =
+    let wInit = Vector.Build.Dense(3,0.0)
+    let classified =  dataPoints |> List.map (fun v -> label_point(wInit, v))
+    let mskl = List.zip correctLabels classified
+                              |> List.filter (fun(y,(x,s)) -> y <> s)
+                              |> List.map (fun (y, (x, s)) -> (x,y))
+
+    learn mskl wInit correctLabels
+    counter
+
+let run_experiment(N) =
+//    """
+//    Runs an experiment with `N` random points.
+//    """
+    let f = find_line(linePoints.[0], linePoints.[1])
+    let correct_labels = dataPoints |> List.map (fun v -> label_point(f, v))
+    let num_iterations = perceptron_learning (List.map (fun (point, s) -> s) correct_labels)
+    num_iterations
+
 [<EntryPoint>]
 let main argv =
-    let N = 100
-    let linePoints = [for i in 1..2 -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0) )]
-    let dataPoints = [for i in 1..N -> (Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0)), i)]
-
-//    let linePoints = [ Vector.Build.Dense([|-0.40877; -0.653459|]);
-//                       Vector.Build.Dense([|-0.579892;  -0.84767|])];
-//
-//
-//    let dataPoints = [ (Vector.Build.Dense([|-0.173793; 0.120863|]), 1);
-//                       (Vector.Build.Dense([|0.308796; -0.758407|]),2);
-//                       (Vector.Build.Dense([|0.841236; -0.780489|]),3);
-//                       (Vector.Build.Dense([|-0.291265; 0.129208|]),4);
-//                       (Vector.Build.Dense([|-0.812164; -0.697282|]),5); ]
-
-    let isLeft (a: Vector<float>, b: Vector<float>, c: Vector<float>) =
-         if ((b.[0] - a.[0])*(c.[1] - a.[1]) - (b.[1] - a.[1])*(c.[0] - a.[0])) > 0.0 then 1.0 else -1.0
-
-
-    let pointsWithSigns = [for (x, i) in dataPoints -> (x, i, isLeft (linePoints.Head,linePoints.Tail.Head, x))]
-    let y =  pointsWithSigns |> List.map (fun (v, i, s) -> s)
-
-    let w = Vector.Build.Dense(2,0.0)
-    let misclasifiedStart = pointsWithSigns
-
-    let rec learn misclasified w =
-        match misclasified with
-            | [] -> true
-            | (x,i,s)::t ->
-                let newW = w + (x * s)
-                printfn "%A" w
-                let result = [ for (x, i) in dataPoints ->  if newW*x > 0.0 then (x,i,1.0) else (x,i,-1.0) ]
-                let mskl = List.zip y result |> List.filter (fun(y1,(x,i,s)) -> y1 <> s) |> List.map (fun (y2, (x,i,s)) -> (x,i,y2))
-
-                learn mskl newW
-
-    learn misclasifiedStart w |> ignore
+    run_experiment(10)
 
     0
--- Homework1/Script.fsx
+++ Homework1/Script.fsx
@@ -3,54 +3,83 @@
 
 open System
 open FSharp.Charting
+open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.LinearAlgebra.Double
 open MathNet.Numerics.Distributions
 
 
 let N = 100
-let linePoints = [for i in 1..2 -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0) )]
-let dataPoints = [for i in 1..N -> (Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0)), i)]
+let linePoints = [|for i in 1..2 -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0) )|]
+let dataPoints = [for i in 1..N -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0) )]
 
-//let linePoints = [ Vector.Build.Dense([|-0.40877; -0.653459|]);
-//                   Vector.Build.Dense([|-0.579892;  -0.84767|])];
+
+let getrandomitem() =
+  let rnd = System.Random()
+  fun (combos : string list) -> List.item (rnd.Next(combos.Length)) combos
+
+let label_point(line: Vector<float>, point: Vector<float>) =
+    if line*point > 0.0 then (point,1.0) else (point,-1.0)
+
+let find_line(point1: Vector<float>, point2: Vector<float>) =
+
+//    Finds the line crossing `point_1` and `point_2`. Returns an array
+//    [w0, w1, w2] representing a line: w0 + w1 * x + w2 * y = 0.
+//    `point_1` and `point_2` are 2-element arrays.
+
+//    # [x1 y1] . [w1] = [-w0] ==> a . x = b
+//    # [x2 y2]   [w2]   [-w0]
+    let w0 = 1.0
+    let (w2, w1) = Fit.Line([|point1.[0]; point2.[0]|], [|point1.[1]; point2.[1]|])
+    Vector.Build.Dense([|w0; w1; w2|])
+
+
+
+
+let perceptron_learning correctLabels =
+    let wInit = Vector.Build.Dense(3,0.0)
+    let classified =  dataPoints |> List.map (fun v -> label_point(wInit, v))
+    let mskl = List.zip correctLabels classified
+                              |> List.filter (fun(y,(x,s)) -> y <> s)
+                              |> List.map (fun (y, x) -> x)
+    let mutable counter = 0
+
+    let rec learn (misclasified: (Vector<float>*float) list) (w: Vector<float>) =
+        match misclasified with
+            | [] -> None
+            | (x,s)::t ->
+                counter <- counter + 1
+                let newW = w + x * Vector.Build.Dense([|1.0;x.[0];x.[1]|])
+                let classifiedNew =  dataPoints |> List.map (fun v -> label_point(newW, v))
+                let msklNew = List.zip correctLabels classifiedNew
+                               |> List.filter (fun(y,(x,s)) -> y <> s)
+                               |> List.map (fun (y, x) -> x)
+                learn msklNew newW
+    learn mskl wInit
+    counter
+
+let run_experiment(N) =
+//    """
+//    Runs an experiment with `N` random points.
+//    """
+    let f = find_line(linePoints.[0], linePoints.[1])
+    let correct_labels = dataPoints |> List.map (fun v -> label_point(f, v))
+    let num_iterations = perceptron_learning (List.map (fun (point, s) -> s) correct_labels)
+    num_iterations
+
+run_experiment(10)
+
+
+//let main()=
+//    for N in [10;100] do //  # N is the number of sample points
+//        results = [run_experiment(N) for _ in range(NUM_RUNS)]
+//        num_iterations, p_f_neq_g = np.mean(results, axis=0)
+//        print('N={}: iter={}, prob={}'.format(N, num_iterations, p_f_neq_g))
+
+
+
 //
-//
-//let dataPoints = [ (Vector.Build.Dense([|-0.173793; 0.120863|]), 1);
-//                   (Vector.Build.Dense([|0.308796; -0.758407|]),2);
-//                   (Vector.Build.Dense([|0.841236; -0.780489|]),3);
-//                   (Vector.Build.Dense([|-0.291265; 0.129208|]),4);
-//                   (Vector.Build.Dense([|-0.812164; -0.697282|]),5); ]
-
-
-let isLeft (a: Vector<float>, b: Vector<float>, c: Vector<float>) =
-     if ((b.[0] - a.[0])*(c.[1] - a.[1]) - (b.[1] - a.[1])*(c.[0] - a.[0])) > 0.0 then 1.0 else -1.0
-
-
-let pointsWithSigns = [for (v, i) in dataPoints -> (v, i, isLeft (linePoints.Head,linePoints.Tail.Head, v))]
-let y =  pointsWithSigns |> List.map (fun (v, i, s) -> s)
-
-let w = Vector.Build.Dense(2,0.0)
-let misclasifiedStart = pointsWithSigns |> List.map (fun (v, i, s) -> (v, i, -1.0))
-let mutable counter = 0
-
-let rec learn misclasified w =
-    match misclasified with
-        | [] -> true
-        | (x,i,s)::t ->
-            counter <- counter + 1
-            let newW = w + x * s
-            let result = [ for (x, i) in dataPoints ->  if newW*x > 0.0 then (x,i,1.0) else (x,i,-1.0) ]
-            let mskl = List.zip y result
-                          |> List.filter (fun(y1,(x,i,s)) -> y1 <> s)
-                          |> List.map (fun (y2, (x,i,s)) -> (x,i,y2))
-            learn mskl newW
-
-learn misclasifiedStart w
-
-counter
-
-Chart.Combine(
-    [ Chart.Line(linePoints |> List.map (fun (v) -> (v.[0], v.[1])))
-      Chart.Point(data = (dataPoints |> List.map (fun (v,i) -> (v.[0], v.[1]))),
-                    Labels = (pointsWithSigns |> List.map (fun (v,i, sign) -> String.Format("{0}({1})",i, sign))))])
+//Chart.Combine(
+//    [ Chart.Line(linePoints |> List.map (fun (v) -> (v.[0], v.[1])))
+//      Chart.Point(data = (dataPoints |> List.map (fun (v,i) -> (v.[0], v.[1]))),
+//                    Labels = (pointsWithSigns |> List.map (fun (v,i, sign) -> String.Format("{0}({1})",i, sign))))])
