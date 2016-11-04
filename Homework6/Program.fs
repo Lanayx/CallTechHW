fcf553f91f12c184f9ec0ed2a97a6683e3562682
Vladimir Schchur <vladimir.shchur@idt.net>
2016-11-04 12:07:49 +0300

    2nd task

--- Homework6/Program.fs
+++ Homework6/Program.fs
@@ -1,14 +1,11 @@
 ﻿open System.IO
 open System
 open MathNet.Numerics.LinearAlgebra
-
-// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+open MathNet.Numerics.Statistics
 
 let regression (dataPoints: (float*float) list) correct_labels  =
     let normalizedPoints = dataPoints |>
                            List.map (fun (x1,x2) -> [|1.0; x1; x2; x1*x1; x2*x2; x1*x2; abs(x1-x2); abs(x1+x2) |])
-
     let x = Matrix.Build.DenseOfRowArrays(normalizedPoints)
     let y = Vector.Build.DenseOfEnumerable(correct_labels)
     let xT = x.Transpose()
@@ -26,20 +23,42 @@ let readData (fileName: string) =
          |> Seq.map (fun arr -> (((float)arr.[0],(float)arr.[1]), (float)arr.[2]))
          |> Seq.toList
 
-let einCalc (dataPoints: (float*float) list) (correct_labels: float list) (w: Vector<float>) =
-    let normalizedPoints = dataPoints |>
-                           List.map (fun (x1,x2) -> [|1.0; x1; x2; x1*x1; x2*x2; x1*x2; abs(x1-x2); abs(x1+x2) |])
-    let x = Matrix.Build.DenseOfRowArrays(normalizedPoints)
-    let y = Vector.Build.DenseOfEnumerable(correct_labels)
-    1.0/((float)dataPoints.Length)*(((x*w - y).L2Norm())**2.0)
+let label_point(line: Vector<float>, point: (float*float)) =
+    let (x1,x2) = point
+    let point3 = vector [1.0; x1; x2; x1*x1; x2*x2; x1*x2; abs(x1-x2); abs(x1+x2)]
+    let mult = point3*line
+    if  mult > 0.0
+    then (point,1.0)
+    else (if mult = 0.0 then (point,0.0) else (point,-1.0))
 
+let calculateP (dataPoints: (float*float) list) correct_labels g =
+    let points = dataPoints
+    let label_g = points |> List.map (fun v -> label_point(g, v))
+    let difference = List.zip correct_labels label_g |> List.map (fun(fs,(g,gs)) -> if fs <> gs then 1.0 else 0.0)
+    Statistics.Mean difference
+
+//let einCalc (dataPoints: (float*float) list) (correct_labels: float list) (w: Vector<float>) =
+//    let normalizedPoints = dataPoints |>
+//                           List.map (fun (x1,x2) -> [|1.0; x1; x2; x1*x1; x2*x2; x1*x2; abs(x1-x2); abs(x1+x2) |])
+//    let x = Matrix.Build.DenseOfRowArrays(normalizedPoints)
+//    let y = Vector.Build.DenseOfEnumerable(correct_labels)
+//    let xwAdjusted = (x*w).Map(fun res -> match res with
+//                                            | a when a > 0.0 -> 1.0
+//                                            | b when b = 0.0 -> 0.0
+//                                            | _ -> -1.0)
+//    let ein = 1.0/((float)dataPoints.Length)*(((xwAdjusted - y).L2Norm())**2.0)
+//    ein
 
 [<EntryPoint>]
 let main argv =
     let testData = readData "../../in.dta"
+    let verificationData = readData "../../out.dta"
     let testX = testData |> List.map (fun (x,y) -> x)
     let testY = testData |> List.map (fun (x,y) -> y)
+    let verifyX = verificationData |> List.map (fun (x,y) -> x)
+    let verifyY = verificationData |> List.map (fun (x,y) -> y)
     let w = regression testX testY
-    let ein = einCalc testX testY w
-    printfn "%A" argv
+    let inP = calculateP testX testY w
+    let outP = calculateP verifyX verifyY w
+    printfn "%A %A" inP outP
     0 // return an integer exit code
