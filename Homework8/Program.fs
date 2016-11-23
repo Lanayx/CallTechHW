8d74b6edf775e94d05d3b66d684821ad61f086d0
Vladimir Shchur <vladimir.shchur@idt.net>
2016-11-23 11:34:25 +0300

    Fix for svm analyzer

--- Homework8/Program.fs
+++ Homework8/Program.fs
@@ -39,7 +39,8 @@ let solveSVM data testData C =
     parameter.Kernel <- SVMKernelType.POLY;
     parameter.Degree <- 2
     parameter.C <- C;
-    //parameter.Gamma <- 1.0;
+    parameter.Coef0 <- 1.0;
+    parameter.Gamma <- 1.0;
 
     let model = SVM.Train(problem, parameter);
     let testResults = testData |> Array.map (fun ((x1,x2),y) -> SVM.Predict(model, [| SVMNode(1,x1); SVMNode(2,x2) |]))
@@ -70,23 +71,23 @@ let main argv =
     let testData = loadData "../../features.test"
 
     //let eins =  [for i in [0.0..1.0..2.0] -> solveSVM (one_vs_all data i) (one_vs_all testData i) 0.01]
-    //let eins =  [for i in [0.0001;0.001;0.01;1.0] -> solveSVM (one_vs_one data 1.0 5.0) (one_vs_one testData 1.0 5.0) i]
+    let eins =  [for i in [0.0001;0.001;0.01;1.0] -> solveSVM (one_vs_one data 1.0 5.0) (one_vs_one data 1.0 5.0) i]
 
-    let initialData = one_vs_one data 1.0 5.0
-    let steps = [1..100] |> List.map (fun step ->
-        let finalResult = [for c in [0.0001;0.001;0.01;0.1;1.0] ->
-                            let chunks = divideForXValidation initialData
-                            let result = [0..9] |> List.map (fun i ->
-                                                let testData = chunks.[i]
-                                                let workData = Array.concat  [| for j in 0..9 -> if i=j then [||] else chunks.[j] |]
-                                                let (err, svmCount) = solveSVM (Array.concat [| workData; chunks.[10] |]) testData c
-                                                err)
-                            let ecv = (List.sum result) / 10.0
-                            (ecv, c)]
-        let x = finalResult |> List.minBy (fun (ecv, c) -> ecv)
-        x )
-    let final = steps |> List.groupBy (fun (ecv,c) -> c) |> List.map (fun (key, group) -> (key, group.Length, (List.average (List.map (fun (ecv,c) -> ecv) group))))
-    printf "%A" final
+//    let initialData = one_vs_one data 1.0 5.0
+//    let steps = [1..100] |> List.map (fun step ->
+//        let finalResult = [for c in [0.0001;0.001;0.01;0.1;1.0] ->
+//                            let chunks = divideForXValidation initialData
+//                            let result = [0..9] |> List.map (fun i ->
+//                                                let testData = chunks.[i]
+//                                                let workData = Array.concat  [| for j in 0..9 -> if i=j then [||] else chunks.[j] |]
+//                                                let (err, svmCount) = solveSVM (Array.concat [| workData; chunks.[10] |]) testData c
+//                                                err)
+//                            let ecv = (List.sum result) / 10.0
+//                            (ecv, c)]
+//        let x = finalResult |> List.minBy (fun (ecv, c) -> ecv)
+//        x )
+//    let final = steps |> List.groupBy (fun (ecv,c) -> c) |> List.map (fun (key, group) -> (key, group.Length, (List.average (List.map (fun (ecv,c) -> ecv) group))))
+//    printf "%A" final
 
     //let eins =  [for i in [0.001;1.0;100.0;10000.0;1000000.0] -> solveSVM (one_vs_one data 1.0 5.0) (one_vs_one testData 1.0 5.0) i]
 
