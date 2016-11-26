c5cde7c28888188960c65ce6d3618e34cc020e14
Vladimir Schchur <vladimir.shchur@idt.net>
2016-11-26 22:20:16 +0300

    11 task

--- FinalExam/SvmGraph.fsx
+++ FinalExam/SvmGraph.fsx
@@ -1,9 +1,24 @@
 ﻿#load @"..\packages\FSharp.Charting.0.90.14\FSharp.Charting.fsx"
-#load @"..\packages\MathNet.Numerics.FSharp.3.14.0-beta03\MathNet.Numerics.fsx"
 
-let data = [((1.0,0.0),-1.0), ((0.0,1.0),-1.0), ((0.0,-1.0),-1.0),
-            ((-1.0,0.0),1.0), ((0.0,2.0),1.0), ((0.0,-2.0),1.0),
+open System
+open FSharp.Charting
+
+let data = [((1.0,0.0),-1.0); ((0.0,1.0),-1.0); ((0.0,-1.0),-1.0);
+            ((-1.0,0.0),1.0); ((0.0,2.0),1.0); ((0.0,-2.0),1.0);
             ((-2.0,0.0),1.0)]
 
 let transform (x1,x2) =
     (x2*x2-2.0*x1-1.0,x1*x1-2.0*x2+1.0)
+
+let transformedData = data |>
+                      List.map (fun (x,y) -> ((transform x),y))
+
+let getLinePoint x1 (w1,w2) b =
+    if w2 = 0.0 then (-b/w1,x1)
+    else (x1,-(b+w1*x1)/w2)
+
+Chart.Combine(
+    [ Chart.Line( [-5.0..0.5..5.0] |> List.map (fun x1 -> getLinePoint x1 (1.0,0.0) -0.5))
+      Chart.Point(data = (transformedData |> List.map (fun (x,y)-> x)),
+                 Labels = (transformedData |> List.map (fun (x,y)-> y.ToString())))])
+
