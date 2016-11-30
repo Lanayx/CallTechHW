16b45546676b2c9bc0ef301fc8df4d90f2abfa9f
Vladimir Shchur <vladimir.shchur@idt.net>
2016-11-30 13:48:40 +0300

    15-18 tasks

--- FinalExam/RBF.fs
+++ FinalExam/RBF.fs
@@ -41,8 +41,7 @@ let rec getNewLLoydClusters (oldCenters: Vector<float> list) (unsupData: Vector<
     else getNewLLoydClusters centers unsupData
 
 let rec LLoydInner K unsupData =
-    let init = [ for i in 1..9 -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0)) ]
-    //let init = [vector [ 0.86966258; 0.40174556]; vector [ 0.96628889; 0.33046748]; vector [-0.80634808; -0.39875312]; vector [ 0.34387146; -0.23656711]; vector [-0.16891906; -0.40718149]; vector [ 0.49933254; -0.49183645]; vector [ 0.27494242; 0.00421283]; vector [-0.08493553; -0.5694352 ]; vector [-0.20150429; -0.28749611]]
+    let init = [ for i in 1..K -> Vector.Build.Random(2, new ContinuousUniform(-1.0,1.0)) ]
     let centers = getNewLLoydClusters init unsupData |> List.toArray
     if (centers.Length <> init.Length)
     then LLoydInner K unsupData
@@ -55,13 +54,13 @@ let LLoyd K gamma (data:((float*float)*float)list) (testData:((float*float)*floa
     let ys = vector (data |> List.map (fun (x,y) -> y))
     let centers = LLoydInner K dataArray
     let fMatrix = Matrix.Build.Dense(data.Length, centers.Length+1, fun i j ->
-        if (j = 0) then 1.0 else exp(gamma*((dataArray.[i]-centers.[j-1]).L2Norm())**2.0))
+        if (j = 0) then 1.0 else exp(-gamma*((dataArray.[i]-centers.[j-1]).L2Norm())**2.0))
     let fP = fMatrix.PseudoInverse()
     let ws = fP*ys
 
     let res = testData |> List.map (fun ((x1,x2),y) ->
                                             Array.zip centers (ws.AsArray() |> Array.skip 1)
-                                            |> Array.map (fun(cent,w) -> w*exp(gamma*((vector [x1;x2] - cent).L2Norm())**2.0))
+                                            |> Array.map (fun(cent,w) -> w*exp(-gamma*((vector [x1;x2] - cent).L2Norm())**2.0))
                                             |> Array.sum)
     let results = res |> List.map (fun x -> sign(x+ws.[0]))
     let labels = testData |> List.map (fun ((x1,x2),y) -> y)
@@ -85,20 +84,39 @@ let SVM gamma data testData =
     e
 
 let RBFRun() =
-    let result = [1..150]
-                |> List.map (fun i ->
-                    let trainData = getData()
-                    let testData = getData()
-                    let svmEin = SVM 1.5 trainData trainData
-                    if (svmEin <> 0.0) then 0.0
-                    else
-                        let svmEout = SVM 1.5 trainData testData
-                        let lloydEout = LLoyd 9 -1.5 trainData testData
-                        (float)(sign(svmEout - lloydEout)))
-    let finalResult = result
-                        |> List.filter (fun elem -> elem <> 0.0)
-                        |> List.take 100
-                        |> List.groupBy (fun elem -> elem)
-                        |> List.map (fun (key, group) -> (key, group.Length))
+//    let result = [1..150]
+//                |> List.map (fun i ->
+//                    let trainData = getData()
+//                    let testData = getData()
+//                    let svmEin = SVM 1.5 trainData trainData
+//                    if (svmEin <> 0.0) then 0.0
+//                    else
+//                        let svmEout = SVM 1.5 trainData testData
+//                        let lloydEout = LLoyd 12 1.5 trainData testData
+//                        (float)(sign(svmEout - lloydEout)))
+//    let finalResult = result
+//                        |> List.filter (fun elem -> elem <> 0.0)
+//                        |> List.take 100
+//                        |> List.groupBy (fun elem -> elem)
+//                        |> List.map (fun (key, group) -> (key, group.Length))
 
-    printf "%A" (finalResult)
+//    let res = [1..1000] |> List.map (fun i ->
+//                            let result = [|1.5;2.0|]
+//                                         |> Array.map (fun gamma ->
+//                                            let trainData = getData()
+//                                            let testData = getData()
+//                                            let lloydEin = LLoyd 9 gamma trainData trainData
+//                                            let lloydEout = LLoyd 9 gamma trainData testData
+//                                            (lloydEin, lloydEout))
+//
+//                            (fst result.[0] > fst result.[1], snd result.[0] > snd result.[1]))
+//                         |> List.groupBy (fun elem -> elem)
+//                         |> List.map (fun (key, group) -> (key, group.Length))
+
+    let res = [1..1000] |> List.map (fun i ->
+                                let trainData = getData()
+                                let lloydEin = LLoyd 9 1.5 trainData trainData
+                                if lloydEin = 0.0 then 1 else 0)
+                       |> List.sum
+
+    printfn "%A" res
